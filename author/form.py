9125339ca81bf4b867180d5ccefc85766e716f5f
Vinmar Tipay <v.tipay@gmail.com>
2017-09-07 17:03:58 +0000

    Added flask-wtforms, validators, macros

---
+++ author/form.py
@@ -0,0 +1,20 @@
+from flask_wtf import Form
+from wtforms import validators, StringField, PasswordField
+from wtforms.fields.html5 import EmailField
+
+class RegisterForm(Form):
+    
+    fullname = StringField('Full Name', [validators.Required()])
+    email = EmailField('Email Address', [validators.Required()])
+    username = StringField('Username', [
+        validators.Required(),
+        validators.Length(min=4, max=25)
+        ]
+    )
+    password = PasswordField('New Password', [
+        validators.Required(),
+        validators.EqualTo('confirm', message='Password must match'),
+        validators.Length(min=6, max=32)    
+        ]
+    )
+    confirm = PasswordField('Repeat Password')
